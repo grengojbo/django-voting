5c2841fb44c18280bf1f66f9d46af560ee15b24f
Oleg Dolya <oleg.dolya@gmail.com>
2013-03-12 11:02:11 +0200

    modify models

--- voting/models.py
+++ voting/models.py
@@ -1,8 +1,10 @@
+# -*- mode: python; coding: utf-8; -*-
 from datetime import datetime
 from django.contrib.contenttypes import generic
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.auth.models import User
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
 try:
     from django.utils.timezone import now
@@ -22,10 +24,12 @@ class Vote(models.Model):
     """
     A vote on an object by a User.
     """
-    user = models.ForeignKey(User, related_name='user')
-    content_type = models.ForeignKey(ContentType)
+    user = models.ForeignKey(User, related_name='user', blank=True, null=True)
+    sessions = models.CharField(_(u'Sessions'), blank=True, null=True)
+    model_view = models.CharField(_(u'Model View'))
+    #content_type = models.ForeignKey(ContentType)
     object_id = models.PositiveIntegerField()
-    object = generic.GenericForeignKey('content_type', 'object_id')
+    #object = generic.GenericForeignKey('content_type', 'object_id')
     vote = models.SmallIntegerField(choices=SCORES)
     created = models.DateTimeField(editable=False, auto_now_add=True)
     last_update = models.DateTimeField(editable=False, auto_now=True)
@@ -35,13 +39,29 @@ class Vote(models.Model):
     class Meta:
         db_table = 'votes'
         # One vote per user per object
-        unique_together = (('user', 'content_type', 'object_id'),)
+        #unique_together = (('user', 'content_type', 'object_id'),)
+        unique_together = (('user', 'model_view', 'object_id'), ('sessions', 'model_view', 'object_id'),)
 
     def __unicode__(self):
-        return u'%s: %s on %s' % (self.user, self.vote, self.object)
+        return u'{0}: {1} on {2}'.format(self.user, self.vote, self.model_view)
 
     def is_upvote(self):
         return self.vote == 1
 
     def is_downvote(self):
         return self.vote == -1
+
+class ViewsObj(models.Model):
+    #user = models.ForeignKey(User, related_name='user', blank=True, null=True)
+    #sessions = models.CharField(_(u'Sessions'), blank=True, null=True)
+    model_view = models.CharField(_(u'Model View'), unique=True)
+    object_id = models.PositiveIntegerField()
+    views = models.PositiveIntegerField(_(u'views'), default=0)
+    last_update = models.DateTimeField(editable=False, auto_now=True)
+
+    class Meta:
+        db_table = 'votes_view'
+
+    def __unicode__(self):
+        return u'{0}: {1}'.format(self.model_view, self.views)
+    
